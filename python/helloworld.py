351b5332b4859947601ded234424ffa1dc784941
Euphemia Zhang <euph.zhang@gmail.com>
2018-06-23 10:52:29 -0400

    "Hello World" python created.

---
+++ python/helloworld.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 23 10:49:17 2018
+
+@author: euphe
+"""
+
+def HelloWorld():
+    print("Hello World!")
+    
+def main():
+    HelloWorld()
+
+if __name__=='__main__' : main()
