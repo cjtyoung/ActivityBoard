67a68cb42175012d9013787953f661f913b0fce9
Euphemia Zhang <euph.zhang@gmail.com>
2018-06-23 11:27:59 -0400

    LunchTime Method has been created.

--- python/helloworld.py
+++ python/helloworld.py
@@ -8,8 +8,12 @@ Created on Sat Jun 23 10:49:17 2018
 def HelloWorld():
     print("Hello World!")
     print("This is Colin")
+
+def LunchTime():
+    print("Let's have some lunch.")
     
 def main():
     HelloWorld()
+    LunchTime()
 
 if __name__=='__main__' : main()
